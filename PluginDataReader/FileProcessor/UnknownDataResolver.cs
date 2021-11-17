4cbdf7772ee09fe1932ce7cf2a2936667ca42fb3
陳鈞 <jim60105@gmail.com>
2021-11-17 09:23:44 +0800

    將未知資料以json倒出

---
+++ PluginDataReader/FileProcessor/UnknownDataResolver.cs
@@ -0,0 +1,67 @@
+﻿using ExtensibleSaveFormat;
+using System.Text.Json;
+
+namespace PluginDataReader.FileProcessor
+{
+    public static class UnknownDataResolver
+    {
+        public static string ResolveUnknownData(PluginData pluginData, string ExtID)
+        {
+            string result = JsonSerializer.Serialize(new Dictionary<string, object>());
+            if (null != pluginData && pluginData.data is Dictionary<string, object> dict)
+            {
+                Console.WriteLine($"\nTry resolve unknown extended data: {ExtID}");
+
+                result = JsonSerializer.Serialize(DeserializeDictionary(dict) as Dictionary<string, object>);
+            }
+
+            Console.WriteLine(result);
+            return result;
+        }
+
+        private static object DeserializeDictionary(dynamic o)
+        {
+            if (o is not IDictionary<string, object> dict
+                || dict.Count == 0
+                || dict.First().Value is not byte[])
+            {
+                return o;
+            }
+
+            Dictionary<string, object> newDict = new();
+            bool tooBig = false;
+            foreach (var kvp in dict)
+            {
+                object tmp = kvp.Value;
+                if (kvp.Value is byte[] b
+                    && b.Length > 0)
+                {
+                    if (b.Length > 1000000)
+                    {
+                        tooBig = true;
+                        break;
+                    }
+                    try
+                    {
+                        tmp = MessagePack.MessagePackSerializer.Deserialize<dynamic>(b);
+                    }
+                    catch (MessagePack.MessagePackSerializationException) { }
+                }
+                tmp = DeserializeDictionary(tmp);
+                newDict.Add(kvp.Key, tmp);
+            }
+
+            if (tooBig)
+            {
+                newDict.Clear();
+                foreach (var kvp2 in dict)
+                {
+                    newDict.Add(kvp2.Key, $"byte[{((byte[])kvp2.Value).Length}]");
+                }
+            }
+
+            return newDict;
+        }
+
+    }
+}
--- PluginDataReader/wwwroot/javascript/index.js
+++ PluginDataReader/wwwroot/javascript/index.js
@@ -21,6 +21,20 @@
             noticeBox.style.left = "-" + viewWidth + "vw";
             noticeTitle.style.left = "0vw";
         }
+    },
+    parseJson: function (json, key) {
+        var element = document.getElementById('div' + key);
+        if (element) {
+            element.appendChild(new JSONFormatter(JSON.parse(json), 5, {
+                hoverPreviewEnabled: false,
+                hoverPreviewArrayCount: 100,
+                hoverPreviewFieldCount: 5,
+                animateOpen: true,
+                animateClose: true,
+                theme: 'dark', // or 'dark'
+                useToJSON: true // use the toJSON method to render an object as a string as available
+            }).render());
+        }
     }
 };
 
